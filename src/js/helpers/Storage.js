c6303d400db176d520afb7be7d2f931d8eadb69c
Premke <mihapremke@gmail.com>
2024-03-06 10:03:10 +0100

    Adds getType #3

--- src/js/helpers/Storage.js
+++ src/js/helpers/Storage.js
@@ -201,6 +201,31 @@ export class StorageHelpers {
         }
     }
 
+	async getType(cid, headers = {}) {
+		const host = this.commonHelpers.getApiHost()
+		const uri = `${host}/operad-ai/api/v1/type?cid=${cid}`
+		const method = 'GET'
+		headers = {
+			...headers,
+			'Accept': 'application/json',
+		}
+		const responseType = null
+		let response
+
+		try {
+			response = await this.commonHelpers.rest(uri, method,
+				headers, responseType, null, true)
+
+			if(response.status > 299) {
+				return this.commonHelpers.newErrorPromise(response)
+			}
+		} catch (error) {
+			return this.commonHelpers.newErrorPromise(error)
+		}
+
+		return this.commonHelpers.newPromise(response)
+	}
+
     async addType(payload, headers = {}) {
         const host = this.commonHelpers.getApiHost()
         const uri = `${host}/operad-ai/api/v1/type`
